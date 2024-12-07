56a8f45212c6e1304e0b39eb77a2f95d657a7354
Hugo-hrabalujeprcak <hugihugh@gmail.com>
2024-12-07 13:55:24 +0100

    Add files via upload

---
+++ MusicGenerator/MusicGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace app {
+    class Program {
+        static void Main(){
+            Console.Write("Počateční frekvence: ");
+            int pocatecniFrekvence = int.Parse(Console.ReadLine());
+            Console.Write("Konečná frekvence: ");
+            int konecnaFrekvence = int.Parse(Console.ReadLine());
+            Console.Write("Krok: ");
+            int krok = int.Parse(Console.ReadLine());
+            Console.Write("Čas: ");
+            int cas = int.Parse(Console.ReadLine());
+            for(int i = pocatecniFrekvence; i<=konecnaFrekvence; i+=krok)
+            {   
+                Console.WriteLine(i);
+                Console.Beep(i,cas);
+            }
+        }
+    }
+}
